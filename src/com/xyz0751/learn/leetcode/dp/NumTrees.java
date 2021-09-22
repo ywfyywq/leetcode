4c3cf767f1df3d64d41fada12a6871d7ff099730
YY <yyclearlife@126.com>
2021-09-22 17:33:03 +0800

    添加题目描述

--- src/com/xyz0751/learn/leetcode/dp/NumTrees.java
+++ src/com/xyz0751/learn/leetcode/dp/NumTrees.java
@@ -1,5 +1,29 @@
 package com.xyz0751.learn.leetcode.dp;
 
+/**
+ * 96. 不同的二叉搜索树
+ *
+ * 难度 中等
+ *
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 3
+ * 输出：5
+ * 示例 2：
+ *
+ * 输入：n = 1
+ * 输出：1
+ *
+ *
+ * 提示：
+ *
+ * 1 <= n <= 19
+ *
+ * @author YY
+ */
 public class NumTrees {
     public static int numTrees(int n) {
         int[] dp = new int[n+1];
--- src/com/xyz0751/learn/leetcode/dp/WordBreak.java
+++ src/com/xyz0751/learn/leetcode/dp/WordBreak.java
@@ -2,6 +2,35 @@ package com.xyz0751.learn.leetcode.dp;
 
 import java.util.*;
 
+/**
+ * 139. 单词拆分
+ *
+ * 难度 中等
+ *
+ * 给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ *
+ * 说明：
+ *
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 示例 1：
+ *
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 示例 2：
+ *
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ *      注意你可以重复使用字典中的单词。
+ * 示例 3：
+ *
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ *
+ * @author YY
+ */
 public class WordBreak {
     public static boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet(wordDict);
