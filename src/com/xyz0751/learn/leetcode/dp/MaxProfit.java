96875fc61cfeb3c999071017668912529e11074d
YY <yyclearlife@126.com>
2021-10-18 17:41:21 +0800

    添加题目描述

--- src/com/xyz0751/learn/leetcode/dp/MaxProfit.java
+++ src/com/xyz0751/learn/leetcode/dp/MaxProfit.java
@@ -31,31 +31,44 @@ package com.xyz0751.learn.leetcode.dp;
  * 1 <= prices.length <= 105
  * 0 <= prices[i] <= 104
  *
+ * 309. 最佳买卖股票时机含冷冻期
+ *
+ * 给定一个整数数组，其中第i个元素代表了第i天的股票价格 。
+ *
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ *
+ * 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ * 示例:
+ *
+ * 输入: [1,2,3,0,2]
+ * 输出: 3
+ * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+ *
+ *
  * @author YY
  */
 public class MaxProfit {
 
     public int maxProfit(int[] prices) {
-        int[] dp = new int[prices.length];
-        dp[prices.length-1] = 0;
-
-        int max = 0;
-        for (int i=prices.length-2; i>=0; i--) {
-            for (int j=prices.length-1; j>i; j--) {
-                if (prices[j] > prices[i]) {
-                    if (j+2<prices.length) {
-                        dp[i] = Math.max(prices[j] - prices[i] + dp[j + 2], dp[i]);
-                    } else {
-                        dp[i] = Math.max(prices[j] - prices[i], dp[i]);
-                    }
-                } else {
-                    dp[i] = Math.max(dp[i], dp[j]);
-                }
-            }
-            max = Math.max(max, dp[i]);
+        if (prices.length == 0) {
+            return 0;
         }
 
-        return max;
+        int n = prices.length;
+        int f0 = -prices[0];
+        int f1 = 0;
+        int f2 = 0;
+        for (int i = 1; i < n; ++i) {
+            int newf0 = Math.max(f0, f2 - prices[i]);
+            int newf1 = f0 + prices[i];
+            int newf2 = Math.max(f1, f2);
+            f0 = newf0;
+            f1 = newf1;
+            f2 = newf2;
+        }
+
+        return Math.max(f1, f2);
     }
 
     public int maxProfit2(int[] prices) {
