aadf17fe60465308b9d9ef94813e83284b3f5e65
YY <yyclearlife@126.com>
2021-11-22 11:49:21 +0800

    207+210
    207. 课程表
    210. 课程表 II

---
+++ src/com/xyz0751/learn/leetcode/graph/CanFinish.java
@@ -0,0 +1,124 @@
+package com.xyz0751.learn.leetcode.graph;
+
+import java.util.*;
+
+/**
+ * 207. 课程表
+ *
+ * 难升  中等
+ *
+ * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+ *
+ * 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+ *
+ * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+ * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：numCourses = 2, prerequisites = [[1,0]]
+ * 输出：true
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+ * 示例 2：
+ *
+ * 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * 输出：false
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成   课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= numCourses <= 105
+ * 0 <= prerequisites.length <= 5000
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * prerequisites[i] 中的所有课程对 互不相同
+ *
+ * @author 力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/course-schedule
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CanFinish {
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+        Map<Integer, Set<Integer>> preMap = new HashMap<>();
+        for (int i=0; i<prerequisites.length; i++) {
+            Set<Integer> set = preMap.getOrDefault(prerequisites[i][0], new HashSet<>());
+            set.add(prerequisites[i][1]);
+            preMap.put(prerequisites[i][0], set);
+        }
+
+        for (int i=0; i<numCourses; i++) {
+            if (isCirculation(new HashSet<>(), preMap, i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isCirculation(Set<Integer> path, Map<Integer, Set<Integer>> preMap, Integer course) {
+        if (path.contains(course)) {
+            return true;
+        }
+        if (!preMap.containsKey(course)) {
+            return false;
+        }
+        path.add(course);
+        Iterator<Integer> it = preMap.get(course).iterator();
+        while (it.hasNext()) {
+            Set<Integer> newPath = new HashSet<>();
+            newPath.addAll(path);
+            if (isCirculation(newPath, preMap, it.next())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v: edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+    }
+
+    public static void main(String[] args) {
+        CanFinish algo = new CanFinish();
+        System.out.println(algo.canFinish(2, new int[][]{{0,1}}));
+        System.out.println(algo.canFinish(3, new int[][]{{1,0},{1,2},{0,1}}));
+        System.out.println(algo.canFinish(2, new int[][]{{1,0},{0,1}}));
+        System.out.println(algo.canFinish(8, new int[][]{{1,0},{2,6},{1,7},{6,4},{7,0},{0,5}}));
+    }
+}
---
+++ src/com/xyz0751/learn/leetcode/graph/FindOrder.java
@@ -0,0 +1,112 @@
+package com.xyz0751.learn.leetcode.graph;
+
+import com.xyz0751.learn.leetcode.utils.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 210. 课程表 II
+ *
+ * 难度 中等
+ *
+ * 现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1。给你一个数组 prerequisites ，其中 prerequisites[i] = [ai, bi] ，表示在选修课程 ai 前 必须 先选修 bi 。
+ *
+ * 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示：[0,1] 。
+ * 返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序，你只要返回 任意一种 就可以了。如果不可能完成所有课程，返回 一个空数组 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：numCourses = 2, prerequisites = [[1,0]]
+ * 输出：[0,1]
+ * 解释：总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
+ * 示例 2：
+ *
+ * 输入：numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+ * 输出：[0,2,1,3]
+ * 解释：总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+ * 因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
+ * 示例 3：
+ *
+ * 输入：numCourses = 1, prerequisites = []
+ * 输出：[0]
+ *  
+ *
+ * 提示：
+ * 1 <= numCourses <= 2000
+ * 0 <= prerequisites.length <= numCourses * (numCourses - 1)
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * ai != bi
+ * 所有[ai, bi] 匹配 互不相同
+ *  
+ *
+ * 拓展：
+ *
+ * 这个问题相当于查找一个循环是否存在于有向图中。如果存在循环，则不存在拓扑排序，因此不可能选取所有课程进行学习。
+ * 通过 DFS 进行拓扑排序 - 一个关于Coursera的精彩视频教程（21分钟），介绍拓扑排序的基本概念。
+ * 拓扑排序也可以通过 BFS 完成。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/course-schedule-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindOrder {
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+    Stack<Integer> stack = new Stack<>();
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        if(valid) {
+            int[] order = new int[numCourses];
+            for (int i=0; i<order.length; i++) {
+                order[i] = stack.pop();
+            }
+            return order;
+        } else {
+            return new int[]{};
+        }
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v: edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+        stack.push(u);
+    }
+
+    public static void main(String[] args) {
+        FindOrder algo = new FindOrder();
+//        ArrayUtils.print(algo.findOrder(2, new int[][]{{0,1}}));
+//        ArrayUtils.print(algo.findOrder(3, new int[][]{{1,0},{1,2},{0,1}}));
+//        ArrayUtils.print(algo.findOrder(2, new int[][]{{1,0},{0,1}}));
+        ArrayUtils.print(algo.findOrder(8, new int[][]{{1,0},{2,6},{1,7},{6,4},{7,0},{0,5}}));
+    }
+}
