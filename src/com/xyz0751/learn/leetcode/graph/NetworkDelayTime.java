e2613104a94f7980e66c18838c30ea8de03f9667
YY <yyclearlife@126.com>
2021-11-25 14:25:02 +0800

    743+1475
    743. 网络延迟时间
    1475. 商品折扣后的最终价格

---
+++ src/com/xyz0751/learn/leetcode/graph/NetworkDelayTime.java
@@ -0,0 +1,85 @@
+package com.xyz0751.learn.leetcode.graph;
+
+import java.util.Arrays;
+
+/**
+ * 743. 网络延迟时间
+ *
+ * 难度 中等
+ *
+ * 有 n 个网络节点，标记为 1 到 n。
+ *
+ * 给你一个列表 times，表示信号经过 有向 边的传递时间。 times[i] = (ui, vi, wi)，其中 ui 是源节点，vi 是目标节点， wi 是一个信号从源节点传递到目标节点的时间。
+ *
+ * 现在，从某个节点 K 发出一个信号。需要多久才能使所有节点都收到信号？如果不能使所有节点收到信号，返回 -1 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：times = [[1,2,1]], n = 2, k = 1
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：times = [[1,2,1]], n = 2, k = 2
+ * 输出：-1
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= k <= n <= 100
+ * 1 <= times.length <= 6000
+ * times[i].length == 3
+ * 1 <= ui, vi <= n
+ * ui != vi
+ * 0 <= wi <= 100
+ * 所有 (ui, vi) 对都 互不相同（即，不含重复边）
+ *
+ * @author 力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/network-delay-time
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NetworkDelayTime {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        final int INF = Integer.MAX_VALUE / 2;
+        int[][] g = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(g[i], INF);
+        }
+        for (int[] t : times) {
+            int x = t[0] - 1, y = t[1] - 1;
+            g[x][y] = t[2];
+        }
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, INF);
+        dist[k - 1] = 0;
+        boolean[] used = new boolean[n];
+        for (int i = 0; i < n; ++i) {
+            int x = -1;
+            for (int y = 0; y < n; ++y) {
+                if (!used[y] && (x == -1 || dist[y] < dist[x])) {
+                    x = y;
+                }
+            }
+            used[x] = true;
+            for (int y = 0; y < n; ++y) {
+                dist[y] = Math.min(dist[y], dist[x] + g[x][y]);
+            }
+        }
+
+        int ans = Arrays.stream(dist).max().getAsInt();
+        return ans == INF ? -1 : ans;
+    }
+
+    public static void main(String[] args) {
+        NetworkDelayTime algo = new NetworkDelayTime();
+        System.out.println(algo.networkDelayTime(new int[][]{ {2,1,1 }, {2,3,1}, {3,4,1}}, 4, 2));
+    }
+}
